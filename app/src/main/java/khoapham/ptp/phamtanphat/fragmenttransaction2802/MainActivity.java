1cd635728a7a1fc2e934ba82c8df35ead9bf7674
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-08 12:27:02 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragmenttransaction2802/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmenttransaction2802/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    public void addAndroid(View v){
+        Toast.makeText(this, "Button add Android", Toast.LENGTH_SHORT).show();
     }
 }
